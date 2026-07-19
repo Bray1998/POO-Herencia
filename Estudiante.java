c1f2d1159116a12b2429481dc28d5e711effaf78
María Teresa Sánchez Martínez <teresanchez051068@gmail.com>
2026-07-19 00:07:34 -0600

    Create Estudiante.java
    “Esta es la subclase Estudiante, que hereda de Persona. Además de los atributos de la clase base, agregué el atributo carrera. Sobrescribí el método mostrarInfo() para que muestre también la carrera.”

---
+++ Estudiante.java
@@ -0,0 +1,15 @@
+java
+public class Estudiante extends Persona {
+    private String carrera;
+
+    public Estudiante(String nombre, int edad, String carrera) {
+        super(nombre, edad);
+        this.carrera = carrera;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Carrera: " + carrera);
+    }
+}
